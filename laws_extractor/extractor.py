a9836237ea5c6268df6a96e3642652e400830ac8
jandersong4 <jandersong4@ufmg.br>
2025-02-23 11:55:35 -0300

    Geração  de csv de todas as provas

--- laws_extractor/extractor.py
+++ laws_extractor/extractor.py
@@ -3,12 +3,16 @@ from utils import pdfTextExtractor, extractQuestions
 import pandas as pd
 
 OAB38 = "oabTests/OAB-38-resolution.pdf"
-filePath = OAB38
-pdf = PdfReader(filePath)
+OAB39 = "oabTests/OAB-39-resolution.pdf"
+OAB40 = "oabTests/OAB-40-resolution.pdf"
+OAB41 = "oabTests/OAB-41-resolution.pdf"
 
-##descomentar essa linha apenas para sobreescrever o arquivo de texto.
-pdfTextExtractor(pdf, "extractedText/oab38extractedText.txt")
-questionAndAnswerList = extractQuestions("extractedText/oab38extractedText.txt")
+filePath = OAB41
+pdf = PdfReader(filePath)
+oabTextNumber = filePath.split('/')[1].split('-')[1]
+
+pdfTextExtractor(pdf, f"extractedText/oab{oabTextNumber}extractedText.txt")
+questionAndAnswerList = extractQuestions(f"extractedText/oab{oabTextNumber}extractedText.txt")
 
 questionsList = []
 commentList = []
@@ -24,4 +28,3 @@ csvName = filePath.split('/')[1].split('.')[0]
 
 df.to_csv(f'csv/{csvName}.csv', index=False)
 df.to_excel(f'excel/{csvName}.xlsx', index=False, engine="openpyxl")
-
